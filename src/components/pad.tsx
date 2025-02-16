84ff07aa64fe5b16bdaf6eeabc0d5c47a10e1f4a
Achille DAVID <davidachille18@gmail.com>
2025-02-16 13:43:53 +0100

    feature(pads): import file to be played on pad

--- src/components/pad.tsx
+++ src/components/pad.tsx
@@ -3,22 +3,49 @@
 import { Volume2 } from "lucide-react";
 import { Button } from "./ui/button";
 import { Howl } from 'howler';
-import { useMemo } from 'react';
+import { Fragment, useMemo, useState } from 'react';
+import { Input } from "./ui/input";
+import { useMutation } from '@tanstack/react-query';
+import { PutBlobResult } from "@vercel/blob";
+import { uploadFile } from "@/lib/files";
 
 export default function Pad() {
+    const [file, setFile] = useState<PutBlobResult | null>(null);
+
     const sound = useMemo(() => new Howl({
-        src: ['/sound.mp3'],
+        src: [file?.url ?? '/sound.mp3'],
         volume: 1,
-    }), []);
+    }), [file]);
 
     const handleSoundPlay = () => {
         sound.stop();
         sound.play();
     };
 
+    const uploadMutation = useMutation({
+        mutationFn: uploadFile,
+        onSuccess: (data: PutBlobResult) => {
+            setFile(data);
+        },
+    });
+
+    const handleFileChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
+        e.preventDefault();
+
+        if (!e.target.files) {
+            throw new Error("No file selected");
+        }
+
+        const file = e.target.files[0];
+        uploadMutation.mutate(file);
+    };
+
     return (
-        <Button size="icon" onClick={handleSoundPlay}>
-            <Volume2 />
-        </Button>
+        <Fragment>
+            <Input type="file" onChange={handleFileChange} accept="audio/*" />
+            <Button size="icon" onClick={handleSoundPlay} disabled={uploadMutation.isPending}>
+                <Volume2 />
+            </Button>
+        </Fragment>
     )
 }
