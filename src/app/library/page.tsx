ccc371bf819c00cccc0c2699848537a221a6eee3
Matthys Boureau <145988911+MatthysBoureau@users.noreply.github.com>
2025-03-21 10:18:27 +0100

    [Library Page]

--- src/app/library/page.tsx
+++ src/app/library/page.tsx
@@ -1,7 +1,14 @@
-export default function Library() {
-  return (
-    <div>
-      <h1>Library</h1>
-    </div>
-  )
+import Library from "@/components/library/library"
+
+export default function LibraryPage() {
+
+    return (
+        <div className="grid grid-flow-row gap-6">
+            <h1 className="text-2xl">Library</h1>
+            <div className="flex flex-col gap-2 bg-slate-200 p-4 rounded-lg">
+                <p>Add files to your libraries</p>
+                <Library />
+            </div>
+        </div>
+    )
 }
