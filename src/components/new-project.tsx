9906dae200d39a08c00305db274cd04cede4f5d0
Fabien <lapertfabien@gmail.com>
2025-05-02 10:12:06 +0000

    feature(project): add new project page

--- src/components/new-project.tsx
+++ src/components/new-project.tsx
@@ -8,12 +8,14 @@ import { useForm } from "react-hook-form";
 import { z } from "zod"
 import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
 import { Input } from "./ui/input";
-import { Dialog, DialogContent, DialogTitle, DialogTrigger } from "./ui/dialog";
-import { useRouter } from "next/navigation";
 import { Switch } from "./ui/switch";
+import { useRouter } from "next/navigation";
+import { Textarea } from "./ui/textarea"; 
+import { Fragment } from "react";
 
 const formSchema = z.object({
-  projectName: z.string().min(2, "name must be at least 2 characters longs").max(50, "name must be at most 50 characters long"),
+  projectName: z.string().min(2, "name must be at least 2 characters long").max(50, "name must be at most 50 characters long"),
+  description: z.string().optional(),
   isPublic: z.boolean().default(false),
 })
 
@@ -24,6 +26,7 @@ export default function NewProject({ userId }: { userId: number }) {
     resolver: zodResolver(formSchema),
     defaultValues: {
       projectName: "",
+      description: "",
       isPublic: false,
     },
   })
@@ -40,58 +43,68 @@ export default function NewProject({ userId }: { userId: number }) {
   function onSubmit(values: z.infer<typeof formSchema>) {
     createMutation.mutate({
       name: values.projectName,
+      description: values.description ?? "",
       isPublic: values.isPublic,
     });
   }
 
   return (
-    <Dialog>
-      <DialogTrigger asChild>
-        <Button size="sm" className="w-fit">create a new project</Button>
-      </DialogTrigger>
-      <DialogContent>
-        <DialogTitle>create a new project</DialogTitle>
-        <Form {...form}>
-          <form onSubmit={form.handleSubmit(onSubmit)} className="flex flex-col gap-4">
-            <FormField
-              control={form.control}
-              name="projectName"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>project name</FormLabel>
-                  <FormControl>
-                    <Input {...field} autoComplete="off" />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            
-            <FormField
-              control={form.control}
-              name="isPublic"
-              render={({ field }) => (
-                <FormItem className="flex flex-row items-center justify-between rounded-lg p-3">
-                  <div className="space-y-0.5">
-                    <FormLabel>Make project public</FormLabel>
-                    <div className="text-sm text-muted-foreground">
-                      Anyone with the link can access this project
-                    </div>
+    <Fragment>
+      <h1 className="text-2xl font-bold mb-4">Create a New Project</h1>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="flex flex-col gap-4">
+          <FormField
+            control={form.control}
+            name="projectName"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Project Name</FormLabel>
+                <FormControl>
+                  <Input {...field} autoComplete="off" />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          
+          <FormField
+            control={form.control}
+            name="description"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Description</FormLabel>
+                <FormControl>
+                  <Textarea {...field} autoComplete="off" />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name="isPublic"
+            render={({ field }) => (
+              <FormItem className="flex flex-row items-center justify-between rounded-lg p-3">
+                <div className="space-y-0.5">
+                  <FormLabel>Make Project Public</FormLabel>
+                  <div className="text-sm text-muted-foreground">
+                    Anyone with the link can access this project
                   </div>
-                  <FormControl>
-                    <Switch
-                      checked={field.value}
-                      onCheckedChange={field.onChange}
-                    />
-                  </FormControl>
-                </FormItem>
-              )}
-            />
-            
-            <Button type="submit">create</Button>
-          </form>
-        </Form>
-      </DialogContent>
-    </Dialog>
+                </div>
+                <FormControl>
+                  <Switch
+                    checked={field.value}
+                    onCheckedChange={field.onChange}
+                  />
+                </FormControl>
+              </FormItem>
+            )}
+          />
+          
+          <Button type="submit">Create</Button>
+        </form>
+      </Form>
+    </Fragment>
   )
 }
