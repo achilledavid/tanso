38d065e860b388fcd6cfa7b004bd9dd0a70c17fe
Achille DAVID <davidachille18@gmail.com>
2025-05-26 10:24:11 +0000

    fix(account): started to upgrade preferences ui

--- src/components/preferences/preferences.tsx
+++ src/components/preferences/preferences.tsx
@@ -1,11 +1,10 @@
 "use client";
 
-import { Button } from "@/components/ui/button/button";
 import { getUserKeyBinding, updateUserKeyBinding } from "@/lib/preferences";
-import { cn } from "@/lib/utils";
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { isEmpty } from "lodash";
 import { Fragment, useEffect, useRef, useState } from "react";
+import style from "./preferences.module.scss"
 
 export default function Preferences({ userId }: { userId: number }) {
   const queryClient = useQueryClient();
@@ -43,7 +42,7 @@ export default function Preferences({ userId }: { userId: number }) {
       queryClient
         .invalidateQueries({ queryKey: ["user-key-binding", userId] })
         .then(() => {
-            setSelectedPad(null);
+          setSelectedPad(null);
         });
     },
   });
@@ -73,26 +72,22 @@ export default function Preferences({ userId }: { userId: number }) {
 
   return (
     <Fragment>
-      <div className="grid grid-cols-4 gap-4 w-full max-w-[550px] bg-white">
+      <div className="grid grid-cols-4 gap-4 w-fit">
         {pads &&
           !isEmpty(pads) &&
           [...pads]
             .sort((a, b) => a.padPosition - b.padPosition)
             .map((pad) => (
               <div key={pad.id} className="flex flex-col gap-2">
-                <Button
-                  size="icon"
-                  variant="outline"
+                <button
+                  className={style.key}
                   onClick={() => handleStartKeyBinding(pad)}
                   onKeyDown={selectedPad?.id === pad.id ? handleKeyBindingChange : undefined}
-                  className={cn(
-                    "relative w-full aspect-square max-w-[120px] max-h-[120px] flex items-center justify-center text-xl"
-                  )}
                 >
                   {selectedPad?.id === pad.id && listeningForKey
                     ? "..."
                     : pad.keyBinding?.toUpperCase() || ""}
-                </Button>
+                </button>
               </div>
             ))}
       </div>
