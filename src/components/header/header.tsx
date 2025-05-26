813831268a1bed2d78d863a9bd841cdd5c9a127e
Matthys Boureau <145988911+MatthysBoureau@users.noreply.github.com>
2025-05-26 21:36:09 +0200

    Add logo svg to Header

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -2,6 +2,7 @@
 
 import style from "./header.module.scss";
 import Link from "next/link";
+import Image from "next/image";
 
 interface HeaderProps {
   children?: React.ReactNode;
@@ -11,7 +12,9 @@ export default function Header({ children }: HeaderProps) {
   return (
     <header className={style.container}>
       <div className={style.content}>
-        <Link href="/" className={style.logo}>Tanso</Link>
+        <Link href="/" className={style.logo}>
+          <Image src={"/svg/logo_white.svg"} alt="Logo" width={140} height={32} />
+        </Link>
         <div className="flex gap-2 items-center ml-auto">
           {children}
         </div>
